445bce09dcbe82ec7eaa5d0fb25abcff98c0c5a8
FrowningCat <vsavinkov60@gmail.com>
2023-11-13 12:40:58 +0300

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
 import Image from 'next/image';
+import search from '../public/search.png';
 
 export default function Home() {
     return (
         <div className="lending">
-            <header>
-                <h2>Hello word</h2>
+            <header className="navigation">
+                <div className="navigation__sections">
+                    <div className="link">Статьи</div>
+                    <div className="link">Тесты</div>
+                    <div className="link">Дополнительные материалы</div>
+                </div>
+                <div className="navigation__searchAndLogin">
+                    <div className="navigation__searchAndLogin__search">
+                        <input
+                            placeholder="Поиск личности..."
+                            type="text"
+                        ></input>
+                        <Image
+                            src={search}
+                            alt="search"
+                            className="navigation__searchAndLogin__search__search"
+                        />
+                    </div>
+                    <div className="button link">Регистрация</div>
+                    <div className="button link">Авторизация</div>
+                </div>
             </header>
         </div>
     );
