002a14d5497ac2c8ddfce7cdb76e9bb3d7c3a4ab
FrowningCat <vsavinkov60@gmail.com>
2023-11-13 10:00:09 +0300

    add style variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
     return (
-        <div>
+        <div className="lending">
             <header>
                 <h2>Hello word</h2>
             </header>
