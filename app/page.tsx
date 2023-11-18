14f01503fb9642245d9ed6b347df48180d26f694
FrowningCat <vsavinkov60@gmail.com>
2023-11-18 11:47:10 +0300

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
 import Image from 'next/image';
-import email from '../public/landing/email.png';
-import tg from '../public/landing/tg.png';
+import headerPhoto from '../public/landing/headerPhoto.png';
 
 export default function Home() {
-    return <div className="lending"></div>;
+    return (
+        <div className="lending">
+            <div className="lending__header">
+                <div className="lending__header__photo">
+                    <Image
+                        src={headerPhoto}
+                        alt="headerPhoto"
+                        className="lending__header__photo__size"
+                    />
+                </div>
+                <div className="lending__header__text">
+                    <div className="lending__header__text__header">
+                        <div className="lending__header__text__header__title">
+                            Aнри Филипп Петен
+                        </div>
+                        <div className="lending__header__text__header__subtitle">
+                            Герой Предатель Патриот
+                        </div>
+                    </div>
+                    <div className="lending__header__text__date">20.11</div>
+                </div>
+            </div>
+        </div>
+    );
 }
