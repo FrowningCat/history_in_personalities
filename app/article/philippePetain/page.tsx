73ead215553c50291065bbb00600930709e71888
FrowningCat <vsavinkov60@gmail.com>
2023-11-24 11:21:24 +0300

    add Petain page

--- app/article/philippePetain/page.tsx
+++ app/article/philippePetain/page.tsx
@@ -1,3 +1,111 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import p1 from '../../../public/person/philippePetain/p5.png';
+import p2 from '../../../public/person/philippePetain/p2.png';
+import p3 from '../../../public/person/philippePetain/p3.png';
+import p4 from '../../../public/person/philippePetain/p4.png';
+
 export default function Petain() {
-    return <div className="petain"></div>;
+    return (
+        <div className="petain">
+            <div className="petain__blokTypeOne">
+                <div className="petain__blokTypeOne__text">
+                    <div className="petain__blokTypeOne__text__title">
+                        Lorem, ipsum dolor.
+                    </div>
+                    <div className="petain__blokTypeOne__text__textual">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptate modi eaque reiciendis, provident reprehenderit
+                        nulla eligendi a atque similique quia voluptatibus quod
+                        architecto quisquam recusandae vel itaque omnis eos.
+                        Eveniet.
+                    </div>
+                </div>
+                <div className="petain__blokTypeOne__img">
+                    <Image
+                        src={p1}
+                        alt="p1"
+                        className="petain__blokTypeOne__img__imgSize linkImg"
+                    />
+                </div>
+            </div>
+            <div className="petain__blokTypeTwo">
+                <div className="petain__blokTypeTwo__img">
+                    <Image
+                        src={p2}
+                        alt="p2"
+                        className="petain__blokTypeTwo__img__imgSize linkImg"
+                    />
+                </div>
+                <div className="petain__blokTypeTwo__text">
+                    <div className="petain__blokTypeTwo__text__title">
+                        Lorem, ipsum dolor.
+                    </div>
+                    <div className="petain__blokTypeTwo__text__textual">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptate modi eaque reiciendis, provident reprehenderit
+                        nulla eligendi a atque similique quia voluptatibus quod
+                        architecto quisquam recusandae vel itaque omnis eos.
+                        Eveniet.
+                    </div>
+                </div>
+            </div>
+            <div className="petain__blokTypeThree">
+                <div className="petain__blokTypeThree__text">
+                    <div className="petain__blokTypeThree__text__title">
+                        Lorem, ipsum dolor.
+                    </div>
+                    <div className="petain__blokTypeThree__text__textual">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptate modi eaque reiciendis, provident reprehenderit
+                        nulla eligendi a atque similique quia voluptatibus quod
+                        architecto quisquam recusandae vel itaque omnis eos.
+                        Eveniet.
+                    </div>
+                </div>
+            </div>
+            <div className="petain__blokTypeOne">
+                <div className="petain__blokTypeOne__text">
+                    <div className="petain__blokTypeOne__text__title">
+                        Lorem, ipsum dolor.
+                    </div>
+                    <div className="petain__blokTypeOne__text__textual">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptate modi eaque reiciendis, provident reprehenderit
+                        nulla eligendi a atque similique quia voluptatibus quod
+                        architecto quisquam recusandae vel itaque omnis eos.
+                        Eveniet.
+                    </div>
+                </div>
+                <div className="petain__blokTypeOne__img">
+                    <Image
+                        src={p3}
+                        alt="p3"
+                        className="petain__blokTypeOne__img__imgSize linkImg"
+                    />
+                </div>
+            </div>
+            <div className="petain__blokTypeTwo">
+                <div className="petain__blokTypeTwo__img">
+                    <Image
+                        src={p4}
+                        alt="p4"
+                        className="petain__blokTypeTwo__img__imgSize linkImg"
+                    />
+                </div>
+                <div className="petain__blokTypeTwo__text">
+                    <div className="petain__blokTypeTwo__text__title">
+                        Lorem, ipsum dolor.
+                    </div>
+                    <div className="petain__blokTypeTwo__text__textual">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptate modi eaque reiciendis, provident reprehenderit
+                        nulla eligendi a atque similique quia voluptatibus quod
+                        architecto quisquam recusandae vel itaque omnis eos.
+                        Eveniet.
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
